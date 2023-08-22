ab1f98b086e6b35af9dee6251f6d5035f4a87a79
Pedro de Lima Birchal <birchalpedro@gmail.com>
2023-08-22 16:28:32 -0300

    Sistema de interação novo
    Novo sistema de interação baseadem distancia e independente de colisão, linhas inutilizadas deletadas objetos redundantes deletados

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helper 
+{
+    public static Transform FindChildWithTag(GameObject parent, string tag)
+    {
+        Transform t = parent.transform;
+        for(int i = 0; i < t.childCount; i++)
+        {
+            if(t.GetChild(i).CompareTag(tag))
+            {
+                return t.GetChild(i);
+            }
+        }
+        return null;
+    }
+}
