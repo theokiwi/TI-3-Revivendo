31d06e4e7cb6a102b224dbd96b3702376e5d8f4a
Theo Diniz <theodordiniz@gmail.com>
2023-10-31 12:19:55 -0300

    Revert "Fix Events"
    This reverts commit 3a7752a9509e0b27e8010e1d83fe8b5d0f05923d.

--- Assets/Scripts/Singletons/Events.cs
+++ Assets/Scripts/Singletons/Events.cs
@@ -14,43 +14,43 @@ public class Events : Singleton<Events>
     currentSeason = Seasons.Standart;
    }
    public void ChangeSeason(int seasonsPassed){
-    switch (currentSeason){
-        case Seasons.Standart:
+    switch (seasonsPassed){
+        case 0:
         currentSeason = Seasons.Standart;
         currentScenario.SetActive(false);
         currentScenario = scenarios[0];
         currentScenario.SetActive(true);
         Debug.Log("Changed 0");
             break;
-        case Seasons.Carnival:
+        case 1:
         currentSeason = Seasons.Carnival;
         currentScenario.SetActive(false);
         currentScenario = scenarios[1];
         currentScenario.SetActive(true);
         Debug.Log("Changed 1");
             break;
-        case Seasons.Easter:
+        case 2:
         currentSeason = Seasons.Easter;
         currentScenario.SetActive(false);
         currentScenario = scenarios[2];
         currentScenario.SetActive(true);
         Debug.Log("Changed 2");
             break;
-        case Seasons.Valentines:
+        case 3:
         currentSeason = Seasons.Valentines;
         currentScenario.SetActive(false);
         currentScenario = scenarios[3];
         currentScenario.SetActive(true);
         Debug.Log("Changed 3");
             break;
-        case Seasons.FestaJulina:
+        case 4:
         currentSeason = Seasons.FestaJulina;
         currentScenario.SetActive(false);
         currentScenario = scenarios[4];
         currentScenario.SetActive(true);
         Debug.Log("Changed 4");
             break;
-        case Seasons.Halloween:
+        case 5:
         currentSeason = Seasons.Halloween;
         currentScenario.SetActive(false);
         currentScenario = scenarios[4];
@@ -58,7 +58,7 @@ public class Events : Singleton<Events>
         Debug.Log("Changed 5");
         
             break;
-        case Seasons.Christmas:
+        case 6:
         currentSeason = Seasons.Christmas;
         currentScenario.SetActive(false);
         currentScenario = scenarios[5];
